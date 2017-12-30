25e904590797d88d5a55565872ca532a39d75291
Matt Haff <matt@craftandcode.io>
2017-12-30 15:15:40 -0500

    adding webpack from sample project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// Import Components
 import App from './components/App';
 
-render(<App/>, document.querySelector('#main'));
+// Import redux stuff
+import { Provider } from 'react-redux';
+import store from './store';
+
+const app = {
+  <Provider store={store}>
+    <App />
+  </Provider>
+}
+
+render(app, document.querySelector('#main'));
