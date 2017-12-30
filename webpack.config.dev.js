2f8e030023f61104a56266993bf909d7164175c6
Matt Haff <matt@craftandcode.io>
2017-12-30 16:20:44 -0500

    omg please work with webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -4,32 +4,32 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'source-map',
   entry: [
-    'webpack-hot-middleware/src',
-    './src/index'
+    'webpack-hot-middleware/client',
+    './client/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/public/'
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin()
   ],
   module: {
     loaders: [
     // js
     {
       test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      loaders: ['babel-loader'],
+      include: path.join(__dirname, 'client')
     },
     // CSS
-    // { 
-    //   test: /\.styl$/, 
-    //   include: path.join(__dirname, 'src'),
-    //   loader: 'style-loader!css-loader!stylus-loader'
-    // }
+    { 
+      test: /\.css$/, 
+      include: path.join(__dirname, 'client'),
+      loader: 'style-loader!css-loader'
+    }
     ]
   }
 };
