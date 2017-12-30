6e3b8f221eeb2a495f9cd0c07847a89bb4fee9f5
Matt Haff <matt@craftandcode.io>
2017-12-30 16:47:15 -0500

    yay it is working

--- client/index.js
+++ client/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// Import CSS
+import css from './styles/style.css';
+
 // Import Components
 import App from './components/App';
 
@@ -8,10 +11,10 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './store';
 
-const app = {
+const app = (
   <Provider store={store}>
     <App />
   </Provider>
-}
+)
 
-render(app, document.querySelector('#main'));
+render(app, document.getElementById('main'));
